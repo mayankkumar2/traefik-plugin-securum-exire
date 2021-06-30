1f34a97f5d12d248faed3b52c64b55841290ce6d
Mayank Kumar <mayank22oct@gmail.com>
2021-06-30 15:56:58 +0530

    refactor: removed debug statements

--- main.go
+++ main.go
@@ -3,12 +3,18 @@ package traefik_plugin_securum_exire
 import (
 	"bytes"
 	"context"
+	"errors"
 	"fmt"
+	"io"
+	"io/ioutil"
 	"net/http"
+	"net/url"
 	"strconv"
+	"time"
 )
 
 type Config struct {
+	Url string `json:"url" yaml:"url" toml:"url"`
 }
 
 
@@ -20,13 +26,15 @@ type SecurumExire struct {
 	http.Handler
 	next http.Handler
 	name string
+	config *Config
 }
 
 func New(ctx context.Context, next http.Handler, config *Config, name string) (http.Handler, error) {
-	fmt.Println("OK runner")
+	// fmt.Println(config)
 	return &SecurumExire{
 		next: next,
 		name: name,
+		config: config,
 	}, nil
 }
 
@@ -44,7 +52,7 @@ func (e *SecurumExireWriter) Header() http.Header {
 }
 
 func (e *SecurumExireWriter) Write(b []byte) (int, error)  {
-	e.contentLength = len(b)
+	e.contentLength += len(b)
 	return e.buffer.Write(b)
 }
 
@@ -52,29 +60,92 @@ func (e *SecurumExireWriter) WriteHeader(statusCode int) {
 	e.statusCode = statusCode
 }
 
+func(e *SecurumExire) CheckIfLeak(endpoint string, b io.Reader) (bool, error) {
+	if e.config.Url == "" {
+		return false, errors.New("error: plugin not configured properly")
+	}
+	urlEndpoint, err := url.Parse(fmt.Sprintf("http://%s:8080/check_endpoint", e.config.Url))
+	if e.config.Url == "" {
+		return false, errors.New("error: plugin not configured properly")
+	}
+	var req = &http.Request{
+		URL: urlEndpoint,
+		Header: map[string][]string{
+			"endpoint": {endpoint},
+			"content-type": {"application/json"},
+		},
+		Body: ioutil.NopCloser(b),
+		Method: http.MethodPost,
+	}
+	cl := http.Client{
+		Timeout: time.Minute*5,
+	}
+	response, err := cl.Do(req)
+	if err != nil {
+		return false, err
+	} else {
+		return response.StatusCode == http.StatusForbidden, nil
+	}
+}
+
 func (e *SecurumExire) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
+	isBlocked, err := e.CheckIfBlocked(req.URL.Path)
+	if err != nil {
+		e.next.ServeHTTP(rw, req)
+		return
+	}
 	respWr := &SecurumExireWriter{
 		ResponseWriter: rw,
 		p:              e,
 	}
-	e.next.ServeHTTP(respWr, req)
-	cl := respWr.contentLength
-	var b = respWr.buffer.Bytes()
-	if len(b) > 100 {
-		fmt.Println("Length more than 100")
-		responseString := []byte("Length More than 100")
-		fmt.Println("length written: ", len(responseString))
-		cl = len(responseString)
+	if !isBlocked {
+		e.next.ServeHTTP(respWr, req)
 	} else {
-		fmt.Println("Length less than 100")
-		l, _ := rw.Write(b)
-		fmt.Println("length written: ", l)
+		rw.WriteHeader(http.StatusNotFound)
+		return
 	}
-	fmt.Println(rw.Header().Values("content-length"))
+	cl := respWr.contentLength
+	buff := new(bytes.Buffer)
+	tee := io.TeeReader(&respWr.buffer, buff)
+	var response []byte
+	var code int
+	isLeak, _ := e.CheckIfLeak(req.URL.Path, tee)
+	if isLeak {
+		response = []byte("{}")
+		code = http.StatusForbidden
+	} else {
+		response, _ = ioutil.ReadAll(buff)
+		code = respWr.statusCode
+	}
+	cl = len(response)
+	//fmt.Println("length written: ", cl)
+	//fmt.Println(rw.Header().Values("content-length"))
 	rw.Header().Del("content-length")
 	rw.Header().Set("content-length", strconv.Itoa(cl))
-	fmt.Println(rw.Header().Values("content-length"))
-	rw.WriteHeader(respWr.statusCode)
-	responseString := []byte("Length More than 100")
-	_, _ = rw.Write(responseString)
+	// fmt.Println(rw.Header().Values("content-length"))
+	_, _ = rw.Write(response)
+	rw.WriteHeader(code)
+}
+
+func (e *SecurumExire) CheckIfBlocked(endpoint string) (bool, error) {
+	if e.config.Url == "" {
+		return false, errors.New("error: plugin not configured properly")
+	}
+	urlEndpoint, _ := url.Parse(fmt.Sprintf("http://%s:8080/check_endpoint", e.config.Url))
+	var req = &http.Request{
+		URL: urlEndpoint,
+		Header: map[string][]string{
+			"endpoint": {endpoint},
+		},
+		Method: http.MethodGet,
+	}
+	cl := http.Client{
+		Timeout: time.Minute*5,
+	}
+	response, err := cl.Do(req)
+	if err != nil {
+		return false, err
+	} else {
+		return response.StatusCode == http.StatusForbidden, nil
+	}
 }
